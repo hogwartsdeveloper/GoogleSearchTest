8f9ae3c5b5b2ecad0425e363f5d31145f1526abe
Zhannur <jannuraidynuly@gmail.com>
2021-07-05 06:53:58 +0600

    add setUp and tearDown methods

--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -1,8 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainPageTest {
     private WebDriver driver;
     private MainPage mainPage;
 
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver",
+                "C:\\Users\\jannu\\IdeaProjects\\TestSelenium\\drivers\\geckodriver.exe");
 
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://google.com/");
+        mainPage = PageFactory.initElements(driver, MainPage.class);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 }
